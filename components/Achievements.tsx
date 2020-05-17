0fa0b7139b8af630f9b291cb25d6a2906cced914
asansyzb <sansyzbayev.alibek@gmail.com>
2020-05-18 05:52:46 +0600

    test

--- components/Achievements.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface Props {}
-
-const Achievements: React.FunctionComponent<Props> = ({}) => {
-  return <div>Achievements</div>;
-};
-
-export default Achievements;
